d0eb10f6a92678d05d6801445aabb8fd3d233df8
Erhan <erhankubanych@gmail.com>
2022-07-18 12:50:54 +0600

    feat: broken down into components

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = (props) => {
+   console.log(props);
+  return (
+     <div className='card'>
+        Card
+        <h2>{props.name}</h2>
+        <p>{props.price}</p>
+     </div>
+  )
+}
+
+export default Card
---
+++ src/components/Foter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Foter = () => {
+  return (
+		<div className="foter">
+			<h1 className="title">Foter</h1>
+		</div>
+	);
+}
+
+export default Foter
