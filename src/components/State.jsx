9d42b0d8a3a853733af5de0186a38fecd238edb1
Erhan <erhankubanych@gmail.com>
2022-07-20 09:20:46 +0600

    feat: changed State

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -2,12 +2,21 @@ import React, {useState} from 'react'
 
 const State = () => {
    const [text, setText] = useState('no click');
+
+   const changed = () => {
+			if (text === "no click") {
+				setText("click");
+			} else {
+				setText("no click");
+			}
+		};
+
   return (
-     <div className='container'>
-        <button onClick={ ()=> setText('click')}>click me</button>
-        <p>{ text }</p>
-     </div>
-  )
+		<div className="container">
+			<button onClick={() => setText(changed)}>click me</button>
+			<p>{text}</p>
+		</div>
+	);
 }
 
 export default State
